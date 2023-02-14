5d6c64d0d33c85cb71c7708cdf22f84aa0f451ac
Valery1306 <valerypamplona13@gmail.com>
2023-02-14 11:45:51 -0500

    Reto 1 de temperaturas

--- src/Reto1.java
+++ src/Reto1.java
@@ -3,7 +3,15 @@ import java.util.Scanner;
 public class Reto1 {
     public static void main(String[] args) {
         Scanner lectura = new Scanner(System.in);
-        int temperaturaf, temperaturac
+        double temperatura1, temperatura2;
+        System.out.println("Ingrese la temperatura en grados centigrados(c°)");
+        temperatura1=lectura.nextDouble();
+        temperatura2= (temperatura1-32)/1.8 ;
+        System.out.println("La temperatura en grados Fahrenheit(F°) es "+temperatura2+"(F°)");
+
+        lectura.close();
+    
     }
     
+
 }
